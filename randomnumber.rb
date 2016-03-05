13bb22d0c8ae21422ecf3ce680458dc0a784ff1b
Abdirazak Ali <prodef@live.com>
2016-03-05 23:57:31 +0000

    random numbers

---
+++ randomnumber.rb
@@ -0,0 +1,39 @@
+#random numbers
+
+#0-0.99
+puts rand
+
+#0-9.99
+puts rand*10
+
+puts (rand*30 + 10).to_i
+
+puts rand(10 + 1)
+
+puts rand(0..10)
+
+puts (0..5).map{rand(0..10)}
+
+srand 1234
+
+puts rand
+
+srand 1234
+
+puts rand
+
+srand 1234
+
+puts rand, rand
+
+srand 1234
+
+puts rand, rand
+
+srand 1234
+
+puts rand(10), rand(100)
+
+srand 1234
+
+puts rand(10), rand(100)
