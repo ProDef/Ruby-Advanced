64e324330e16e6bdd32db266c2ee345604273ff5
Abdirazak Ali <prodef@live.com>
2016-03-11 15:26:32 +0000

    update operator overloading comparable

--- operatoroverloadingcomparable.rb
+++ operatoroverloadingcomparable.rb
@@ -6,6 +6,8 @@ def <=>(other)
 	self.name<=>other.name
 end
 
+#3 possible out comes -1, 0, 1
+#if the left is less than the right 1, equal 0, or the right is less it's 1
 puts "abdi"<=>"zak"
 
 
@@ -22,10 +24,17 @@ end
 
 end
 
-abdi = Alpha.new("abdi")
-zorro = Alpha.new("zorro")
+abdi = Alpha.new(12)
+zorro = Alpha.new(7)
 
+#abdi is greater so thats true
 puts abdi > zorro
+
 puts abdi < zorro
+
 puts abdi == zorro
+#they are not equal
 puts abdi != zorro
+
+# 1 because abdi is greater than zorro
+puts abdi<=>zorro
