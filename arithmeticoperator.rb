b246d9fc8ffc2c27e1e3b7dc2cf2158bf8fdd08f
Abdirazak Ali <prodef@live.com>
2016-03-12 13:01:49 +0000

    arithmetic operator overloading

---
+++ arithmeticoperator.rb
@@ -0,0 +1,48 @@
+#operator overloading with + / * % **
+
+
+class Pet
+
+attr_accessor :num
+
+def initialize(num)
+	@num = num
+end
+
+#addition operator
+def +(other)
+	self.num + other.num
+end
+
+#multiplication operator
+def *(other)
+	self.num * other.num
+end
+
+#division operator
+#to get the float the object needs to have a float number
+def /(other)
+	self.num / other.num
+end
+
+#
+def %(other)
+	self.num % other.num
+end
+
+#exponetial operator
+def **(other)
+	self.num ** other.num
+end
+
+end
+
+a = Pet.new(3)
+b = Pet.new(5)
+
+
+puts a + b
+puts a * b
+puts b / a
+puts a % b
+puts a ** b
