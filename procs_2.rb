bdcb27a1fff05899197dd0c8fa14083977f0c160
Abdirazak Ali <prodef@live.com>
2016-03-28 16:01:27 +0100

    blocks into objects the implicit way and easy lambda

---
+++ procs_2.rb
@@ -0,0 +1,21 @@
+# implicit ways - two ways
+# 	-using either the yeild statement
+# 	-using concept of &block
+
+
+def school(&teachers)
+	puts "the school has teachers"
+
+	teachers.call
+	return teachers
+end
+
+
+block = school{ puts "yes, they have many episodes"}
+block.call
+
+
+# one simple way to create lamda 
+pupil = lambda { |var1, var2| puts "yes we have both #{var1} and #{var2} going to this school"}
+
+pupil.call("abdi", "hamza")
